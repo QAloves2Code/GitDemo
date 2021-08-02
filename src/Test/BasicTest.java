4433ae7bca611176e6fa83fcfebbe34cc74a03d1
Gokul.UdayaKumar <qaloves2code@gmail.com>
2021-08-02 20:46:24 +0530

    This is my first Commit

---
+++ src/Test/BasicTest.java
@@ -0,0 +1,24 @@
+package Test;
+
+public class BasicTest {
+
+	public static void main(String[] args) {
+		
+		
+		//Adding single program
+		
+		String s ="Hello";
+		
+		if(s.equals("Hello"))
+		{
+			System.out.println("I am printed");
+		}
+		
+		else
+		{
+			System.out.println("I am else statement");
+		}
+
+	}
+
+}
